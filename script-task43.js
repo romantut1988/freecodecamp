f37e518c2c425ec3ee2e7fc96ea56b3bac76bbf7
romantut1988 <romantut1988@mail.ru>
2021-10-18 12:30:35 +0300

    task-43

---
+++ script-task43.js
@@ -0,0 +1,7 @@
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+var myArray = [["John", 23], []];
+var removedFromMyArray =  myArray.pop();
+var removedFromMyArray =  ["cat", 2];
