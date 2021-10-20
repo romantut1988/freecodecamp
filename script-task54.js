4c326f04cdddd44887274dd8f07af978db5415b7
romantut1988 <romantut1988@mail.ru>
2021-10-21 01:19:36 +0300

    Assignment with a returned value

---
+++ script-task54.js
@@ -0,0 +1,17 @@
+// Setup
+var changed = 0;
+
+function change(num) {
+    return (num + 5) / 3;
+}
+
+changed = change(10);
+
+// Setup
+var processed = 2;
+
+function processArg(num) {
+    return (num + 3) / 5;
+}
+processed = processArg(7);
+// Only change code below this line
