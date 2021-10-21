4349819b9f72134064d5a7461c71a4b69c27a974
romantut1988 <romantut1988@mail.ru>
2021-10-21 09:36:05 +0300

    Stand in line

---
+++ script-task55.js
@@ -0,0 +1,12 @@
+function nextInLine(arr, item) {
+
+    var queue = arr.push(item);
+    var removeItem = arr.shift();
+    return removeItem;  
+}
+
+var testArr = [1,2,3,4,5];
+
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6)); // Modify this line to test
+console.log("After: " + JSON.stringify(testArr));
